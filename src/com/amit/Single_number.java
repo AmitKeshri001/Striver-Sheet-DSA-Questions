f1e6ca121c22e962a81a129b94c701ba07d55694
Amit001 <ak8340404187@gmail.com>
2026-08-05 19:29:43 +0530

    Single number

---
+++ src/com/amit/Single_number.java
@@ -0,0 +1,26 @@
+package com.amit;
+
+import java.util.Scanner;
+
+public class Single_number
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array : ");
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        int result = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            result ^= arr[i];
+        }
+        System.out.println("the ans is :  " + result);
+    }
+}
