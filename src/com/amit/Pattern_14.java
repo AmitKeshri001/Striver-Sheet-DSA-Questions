fc648017755e581ff95ce880ebcb8d7ef066606e
Amit001 <ak8340404187@gmail.com>
2026-08-14 23:31:35 +0530

    Pattern 13

---
+++ src/com/amit/Pattern_14.java
@@ -0,0 +1,50 @@
+package com.amit;
+import java.util.*;
+public class Pattern_14
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the No. of rows : ");
+        int r = sc.nextInt();
+
+        for(int i=1;i<=r;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+
+            for(int k=0;k<2*(r-i);k++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for(int i=r-1;i>=0;i--)
+        {
+            for(int j=i;j>0;j--)
+            {
+                System.out.print("*");
+            }
+            for(int k=1;k<=2*(r-i);k++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j=i;j>0;j--)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
